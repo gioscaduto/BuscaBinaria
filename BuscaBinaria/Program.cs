207cfa4bdee576550d3a409eb8f13f1e5d5af032
Giovani <gscaduto@sabesp.com.br>
2020-01-28 15:10:54 -0300

    First Commit

---
+++ BuscaBinaria/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace BuscaBinaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro[] carros =
+            {
+                new Carro("Ferrari", 500_000),
+                new Carro("Hilux", 150_000),
+                new Carro("Gol", 40_000),
+                new Carro("Onix", 40_000),
+                new Carro("Mobi", 30_000),
+                new Carro("Strada", 45_000),
+                new Carro("EcoSport", 71_000)
+            };
+
+            QuickSort.Ordena(carros, 0, carros.Length);
+            ImprimeCarros(carros, "Carros Ordenados");
+
+            int posicaoEncontrada = Busca(carros, 0, carros.Length, 45_000);
+            
+            if(posicaoEncontrada >= 0)
+            {
+                Console.WriteLine
+                    ($"Carro com Preço igual à { carros[posicaoEncontrada].Preco.ToString("C") } está na posição: { posicaoEncontrada }");
+            }
+            else
+            {
+                Console.WriteLine("Preço não encontrado");
+            }
+
+            Console.ReadKey();
+        }
+
+        private static int Busca(Carro[] carros, int de, int ate, decimal preco)
+        {
+            int meio = (de + ate) / 2;
+
+            if(de > ate) // Preco nao encontrado
+            {
+                return -1;
+            }
+
+            Carro carroMeio = carros[meio];
+
+            if (preco == carroMeio.Preco)
+            {
+                return meio;
+            }
+
+            if(preco < carroMeio.Preco) // Esquerda
+            {
+                return Busca(carros, de, meio - 1, preco);
+            }
+
+            // Direita
+            return Busca(carros, meio + 1, ate, preco);
+        }
+
+        private static void ImprimeCarros(Carro[] carros, string titulo)
+        {
+            Console.WriteLine(titulo);
+            Console.WriteLine();
+            foreach (var carro in carros)
+            {
+                Console.WriteLine(carro.ToString());
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ BuscaBinaria/QuickSort.cs
@@ -0,0 +1,50 @@
+﻿namespace BuscaBinaria
+{
+    public static class QuickSort
+    {
+        public static void Ordena(Carro[] carros, int de, int ate)
+        {
+            int elementos = ate - de;
+            int posicaoPivo;
+
+            if (elementos > 1)
+            {
+                posicaoPivo = Particiona(carros, de, ate);
+                Ordena(carros, de, posicaoPivo);
+                Ordena(carros, posicaoPivo + 1, ate);
+            }
+        }
+
+        private static int Particiona(Carro[] carros, int inicio, int termino)
+        {
+            int menoresEncontrados = inicio;
+
+            Carro pivo = carros[termino - 1];
+            Carro atual;
+
+            for (int analisando = inicio; analisando < termino - 1; analisando++)
+            {
+                atual = carros[analisando];
+
+                if (atual.Preco <= pivo.Preco)
+                {
+                    Troca(carros, analisando, menoresEncontrados);
+                    menoresEncontrados++;
+                }
+            }
+
+            Troca(carros, termino - 1, menoresEncontrados);
+
+            return menoresEncontrados;
+        }
+
+        private static void Troca(Carro[] carros, int de, int para)
+        {
+            Carro carro1 = carros[de];
+            Carro carro2 = carros[para];
+
+            carros[para] = carro1;
+            carros[de] = carro2;
+        }
+    }
+}
